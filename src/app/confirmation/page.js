029105dc863977d87801cea50baefbf3a3d2a41d
Shelendra <singhshelendra70@gmail.com>
2024-10-19 11:07:08 +0530

    'returnupload'

---
+++ src/app/confirmation/page.js
@@ -0,0 +1,16 @@
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import Confirmation from '@/components/pages/carComponent/Confirmation';
+import React from 'react'
+
+const ConfrimationComponent = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Confirmation/>
+        <Footer />
+    </div>
+  )
+}
+
+export default ConfrimationComponent;
---
+++ src/components/pages/carComponent/Confirmation.jsx
@@ -0,0 +1,13 @@
+"use client"
+import React from "react";
+
+const Confirmation = () => {
+  return (
+    <div className="container">
+      <h2>Thank you for your purchase!</h2>
+      <p>Your order has been placed successfully. You will receive a confirmation email shortly.</p>
+    </div>
+  );
+};
+
+export default Confirmation;

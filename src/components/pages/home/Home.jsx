9e5f7dc50b962af7605fddc9f18d59a6217453e7
Nitesh403 <nitesh@hucpl.com>
2024-12-26 16:08:02 +0530

    about page done

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -8,6 +8,7 @@ import { useDispatch } from "react-redux";
 import React, { useRef } from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
 import { HiArrowLongRight } from "react-icons/hi2";
+import { Autoplay } from 'swiper/modules';
 
 import "swiper/css";
 import "swiper/css/effect-coverflow";
@@ -16,6 +17,7 @@ import "swiper/css/navigation";
 
 import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
 import OurAchievement from '@/components/OurAchievment';
+import Footer from '@/components/Footer';
 
 
 
@@ -416,6 +418,11 @@ const Home = () => {
                         modifier: 2.5,
                       }
                     }
+                    autoplay={{
+                      delay: 2500,
+                      disableOnInteraction: false,
+                    }}
+                  
                     pagination={{
                       el: ".swiper-pagination", clickable: true
                     }}
@@ -424,7 +431,7 @@ const Home = () => {
                       prevEl: "swiper-button-prev",
                       clickable: true,
                     }}
-                    modules={[EffectCoverflow, Pagination, Navigation]}
+                    modules={[EffectCoverflow, Pagination, Navigation, Autoplay]}
                     className="swiper_container"
                   >
                     <SwiperSlide>
@@ -444,12 +451,6 @@ const Home = () => {
                     </SwiperSlide>
 
                     <div className="slider-controler">
-                      {/* <div className="swiper-button-prev slider-arrow">
-           <ion-icon name="arrow-back-outline"></ion-icon>
-            </div>
-            <div className="swiper-button-next slider-arrow">
-           <ion-icon name="arrow-forword-outline"></ion-icon>
-            </div> */}
                       <div className="swiper-pagination"></div>
                     </div>
                   </Swiper>
@@ -498,6 +499,7 @@ const Home = () => {
       <section>
         <OurAchievement />
       </section>
+     
     </>
 
   )

052ed4acb898785db48bf6bb89c5c1cfd8a1a231
Nitesh403 <nitesh@hucpl.com>
2025-02-13 11:43:32 +0530

    instagram reels

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -360,7 +360,7 @@ const Home = () => {
                         <div className="d-flex justify-content-between">
                           {groupedItems.map((videoItem, videoIndex) => (
                             <div className="carousel-item-video" key={videoIndex}>
-                              <video width="100%" height="500" controls>
+                              <video width="100%" height="400" controls>
                                 <source src={videoItem.media_url} type="video/mp4" />
                                 Your browser does not support the video tag.
                               </video>
